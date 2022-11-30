23fe82c947dbd476089a428b383524e9f7926633
ezequielrosa <ezequielrosa@gmail.com>
2022-11-29 23:30:07 -0300

    Update find_substring_with_n_different_chars.py

--- algorithm/examples/sliding_window/find_substring_with_n_different_chars.py
+++ algorithm/examples/sliding_window/find_substring_with_n_different_chars.py
@@ -38,8 +38,8 @@
 	1.	Como resultado vamos a devolver la subcadena que haya quedado dentro de la ventana cuando esta fué más grande
 	2.	La ventana crece mientras esté compuesta por hasta p cantidad de caracteres distintos
 	3.	Cada caracter distinto va a ser un elemento del conjunto, una llave clave del contador counter(dict)
-	4.	Utilizamos dos punteros auxiliares para registrar el primer momento máximo de la ventana
-	5.	Cuando la cantidad de caracteres en la ventana es superior a p la mantenemos en tamaño y la seguimos avanzando
+	4.	Utilizamos dos punteros auxiliares para registrar el momento y tamaño máximo de la ventana
+	5.	Cuando la cantidad de caracteres en la ventana es superior a p mantenemos su tamaño y seguimos avanzando
 	6.	Cada vez que la cantidad de caracteres que componen la cadena cambia verificamos si se vuelve a cumplir la condición p
 	7.	El proceso no termina hasta que la ventana no haya llegado al final de la cadena
 	
@@ -56,48 +56,56 @@ def sliding_window_template_with_examples(s, p):
     # en este caso necesitamos llevar un conjunto con hasta p + 1 cantidad de elementos
     counter = Counter(p)
 
-    # los dos punteros que indican los límites de la ventana deslizante
+    # los dos punteros que indican los extremos de la ventana deslizante
     start, end = 0, 0
-	# los dos punteros que indican los límites dasde y hasta de la subcadena resultado
-	# es decir la ventana más grande que solo tuvo p cantidad de caracteres
-	desde, hasta = 0, 0
+    # los dos punteros que indican desde y hasta donde fue más grande la ventana
+    # es decir la ventana más grande que solo tuvo p cantidad de caracteres
+    desde, hasta = 0, 0
     # condicion de corte, se actualiza cada vez que cambia alguna clave, el valor inicial depende del caso
     # en nuestro caso la cantidad de caracteres distintos que hay en la ventana
     # mientras sea menor o igual a p ampliamos la ventana
     count = 0
     # resultado, se devuelve un entero (en caso de máximo o mínimo) o una lista (con todos los índices)
-    res = 0
+    r = 0
 
     # recorrer la cadena s de principio a fin
     while end < len(s):
+	# incrementamos el contador del caracter actual en la colección counter
         counter[s[end]] += 1
-        # actualiza el contador según alguna condición
+
+	'''
+	# actualiza el contador según alguna condición
         if counter[s[end]] > 1:
             count += 1
         end += 1
-
+	'''
+	
         # condición del contador
-        while count > 0:
+        while len(counter) > p:
             '''
             acá actualizamos res si estamos buscando un mínimo 
             '''
-
+	    # si hay más de p caracteres distintos mantenemos el tamaño de la ventana
             # avanzamos el puntero start para hacerlo inválido o válido nuevamente
+	    # restamos la cantidad del caracter que hay en start porque ya no pertenece a la ventana
             counter[s[start]] -= 1
             # update count based on some condition
-            if counter[s[start]] > 0:
-                count -= 1
+            if counter[s[start]] == 0:
+                del counter[s[start]]
             start += 1
-
         '''
         acá actualizamos res si estamos buscando un máximo
-        
-        
-        
         '''
-        res = max(res, end - start)
 
-    return res
+	if hasta - desde < end - start
+	    hasta = end
+	    desde = start
+	
+	end += 1
+	'''
+        res = max(res, end - start)
+	'''
+    return s[desde:hasta + 1]
 
 # para mayores referencias dirigirse a leetcode
 # https://leetcode.com/problems/minimum-window-substring/discuss/26808/here-is-a-10-line-template-that-can-solve-most-substring-problems
