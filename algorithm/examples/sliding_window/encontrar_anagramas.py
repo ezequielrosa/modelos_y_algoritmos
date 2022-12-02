10e093d9d30ba6c303b58432cb32f20f94ce054c
ezequielrosa <ezequielrosa@gmail.com>
2022-12-02 18:15:42 -0300

    Update and rename encontrar_subcadenas_compuestas_por_caracteres_de_otra to encontrar_anagramas.py

--- algorithm/examples/sliding_window/encontrar_anagramas.py
+++ algorithm/examples/sliding_window/encontrar_anagramas.py
@@ -1,17 +1,23 @@
 '''
 	EJEMPLO DE CASO DE USO DEL ALGORITMO DE VENTANA DESLIZANTE
-	subcadena más larga que contiene p cantidad de caracteres distintos
+	todas las subcadenas de una cadena que son una permutación de otra cadena
 	-----------------------------------
-	Problema:	Dada una cadena s encontrar la subcadena más larga que contiene p cantidad de caracteres distintos.
-	Entrada:	Dada una string s y un número positivo p.
-	Salida: 	Buscar la subcadena r más larga dentro de la cadena s que contiene p cantidad de caracteres distintos. 
+	Problema:	Encontrar todas las subcadenas de una cadena que contiene todos los caracteres de otra cadena. 
+			En otras palabras, buscar todas las subcadenas de la primera cadena que sean anagramas de la segunda cadena.
+			Tenga en cuenta que el problema se dirige específicamente subcadenas contíguas (es decir, ocupan posiciones consecutivas) 
+			e inherentemente mantienen el orden de los elementos.
+	Entrada:	Dada una string s y otra string p.
+	Salida: 	Buscar el conjunto result más larga dentro de la cadena s que contiene p cantidad de caracteres distintos. 
 			Si p es mayor que el número total de caracteres distintos en la string, r devuelve la string completa.
 	El problema difiere del problema de encontrar la subsecuencia más larga con cantidad p caracteres distintos. 
 	A diferencia de las subsecuencias, subcadenas se requiere que ocupen posiciones consecutivas dentro de la string original.
-	Por ejemplo, considerando la string ‘abcbdbdbbdcdabd’
-	Para p = 2, r sería ‘bdbdbbd’
-	Para p = 3, r sería ‘bcbdbdbbdcd’
-	Para p = 5, r sería ‘abcbdbdbbdcdabd’
+
+	Por ejemplo, considerando la cadena s = 'XYYZXZYZXXYZ'
+	Para una cadena patrón p = 'XYZ'
+	Anagrama sería 'YZX' presente en el índice 2
+	Anagrama sería 'XZY' presente en el índice 4
+	Anagrama sería 'YZX' presente en el índice 6
+	Anagrama sería 'XYZ' presente en el índice 9
 	-----------------------------------
 	El algoritmo de ventana deslizante se utiliza para encontrar subcadenas o para problemas de máximos y mínimos.
 	Utiliza dos punteros como límites de una ventana deslizable
@@ -28,15 +34,14 @@
 	4.	Utilizamos dos punteros auxiliares para registrar el momento del tamaño máximo de la ventana
 	5.	Cuando la cantidad de caracteres en la ventana es superior a p la achicamos
 	6.	Cada vez que la cantidad de caracteres que componen la cadena cambia verificamos si se vuelve a cumplir la condición p
-	7.	El proceso no termina hasta que la ventana no haya llegado al final de la cadena
-	
+	7.	El proceso no termina hasta que la ventana no haya llegado al final de la cadena	
 '''
 
 from collections import Counter
 
-def encontrar_mayor_subcadena_con_p_diferentes_caracteres(s, p):
+def encontrar_anagramas(s, p):
 	# s es la cadena de entrada (string)
-	# p es el patrón que condiciona la búsqueda en este caso un entero cantidad
+	# p es el patrón que condiciona la búsqueda en este caso otra cadena
 	# contador es una colección que sirve de conjunto y contador
 	contador = Counter()
 	# vent_inicio es el puntero límite inferior de la ventana
